6cdef606a43355903cd7d6fd410a1102cc0542a9
Wahyu Esya <wahyu.esya17@gmail.com>
2024-08-01 13:25:46 +0700

    fix: add container layout error pages

--- src/pages/NotFoundPages/index.jsx
+++ src/pages/NotFoundPages/index.jsx
@@ -1,17 +1,20 @@
 import { House } from "lucide-react";
 import styles from "../styles/NotFoundPages.module.css";
 import { Link } from "react-router-dom";
+import { ContainerLayout } from "../../components/layouts";
 
 const NotFoundPages = () => {
   return (
-    <div className={styles.container}>
-      <h1>404</h1>
-      <h2>Page not found</h2>
-      <Link to="/" className={styles.btn}>
-        <p>Back to Home</p>
-        <House size={20} />
-      </Link>
-    </div>
+    <ContainerLayout>
+      <div className={styles.container}>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <Link to="/" className={styles.btn}>
+          <p>Back to Home</p>
+          <House size={20} />
+        </Link>
+      </div>
+    </ContainerLayout>
   );
 };
 
