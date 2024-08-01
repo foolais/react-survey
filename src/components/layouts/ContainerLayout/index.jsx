42befc4c0a94d463ebb24c0ba96f786d25fdacd7
Wahyu Esya <wahyu.esya17@gmail.com>
2024-08-01 12:25:20 +0700

    feat: error pages

---
+++ src/components/layouts/ContainerLayout/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import "../styles/ContainerLayout.module.css";
+
+const ContainerLayout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default ContainerLayout;
--- src/pages/NotFoundPages.jsx
+++
@@ -1,5 +0,0 @@
-const NotFoundPages = () => {
-  return <div>NotFoundPages</div>;
-};
-
-export default NotFoundPages;
